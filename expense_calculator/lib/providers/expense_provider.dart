2605f1f3d87687f96a12204a42c559f95b2568ab
bithe <sharminbithe2001@gmail.com>
2026-08-14 11:46:37 +0600

    Fix expense model and provider

--- expense_calculator/lib/providers/expense_provider.dart
+++ expense_calculator/lib/providers/expense_provider.dart
@@ -2,13 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/expense.dart';
 
 class ExpenseProvider {
-  final FirebaseFirestore_firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   List<Expense> _expenses = [];
+
   List<Expense> get expenses => _expenses;
+
   Stream<List<Expense>> getExpenses() {
     return _firestore.collection('expenses').snapshots().map((snapshot) {
-      return snapshot.docs.map((doc) => Expense.fromFirestore(doc)).toList();
+      final expenses = snapshot.docs
+          .map((doc) => Expense.fromFirestore(doc))
+          .toList();
+
+      _expenses = expenses;
+      return expenses;
     });
   }
 }
