eb1edb68a8f6d80ae4099e8671818a6b45a40002
Nipa2210 <nipaakterratri2001@gmail.com>
2026-08-14 18:52:07 +0600

    Fstore_Database Created

--- expense_calculator/lib/providers/expense_provider.dart
+++ expense_calculator/lib/providers/expense_provider.dart
@@ -1,15 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import '../models/expense.dart';
 
-class ExpenseProvider {
+class ExpenseProvider extends ChangeNotifier {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   List<Expense> _expenses = [];
   List<Expense> get expenses => _expenses;
-  
+
   Stream<List<Expense>> getExpenses() {
     return _firestore.collection('expenses').snapshots().map((snapshot) {
-      return snapshot.docs.map((doc) => Expense.fromFirestore(doc)).toList();
+      final expenses = snapshot.docs.map((doc) => Expense.fromFirestore(doc)).toList();
+      _expenses = expenses;
+      notifyListeners();
+      return _expenses;
     });
   }
 }
